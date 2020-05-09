2d21d3634f026f5603d301c2418ec9e291dca432
JohnEganLIT <johnegan013@gmail.com>
2020-05-09 16:50:57 +0100

    Uploaded SimpleServlet

---
+++ SimpleServlet/src/java/SimpleServlet.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author John
+ */
+@WebServlet(urlPatterns = {"/SimpleServlet"})
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>First Servlet Example</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Aren't Servlets Great?</p>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+
+}
